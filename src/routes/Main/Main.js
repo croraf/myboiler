b19f6793926bf0ece69390ef18f4116e17a05730
Korisnik <vrafaeli@msn.com>
2017-05-25 21:22:15 +0200

    Shortening example

--- src/routes/Main/Main.js
+++ src/routes/Main/Main.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import {Grid, Row, Col} from 'react-bootstrap';
 
 
 class Main extends React.Component {
     render () {
         return (
-            <Grid>
-                <Row>
-                </Row>
-            </Grid>
+            <div />
             
         );
     }
